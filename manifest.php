961a09db8630a92d63b20dba40cc8f15e6bc5d32
jonmlas <jonmlas@gmail.com>
2025-08-21 18:58:00 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -2,13 +2,28 @@
 
 $manifest = array();
 
-$manifest['name'] = __('Mailer', 'fw');
-$manifest['description'] = __('This extension will let you set some global email options and it is used by other extensions (like Forms) to send emails.', 'fw');
-$manifest['version'] = '1.2.12';
-$manifest['standalone'] = false;
-$manifest['display'] = false;
-$manifest['github_update'] = 'ThemeFuse/Unyson-Mailer-Extension';
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Mailer-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/forms/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+$manifest['name']        = __( 'Mailer', 'fw' );
+$manifest['slug']        = 'unysonplus-mailer';
+$manifest['description'] = __(
+	'This extension will let you set some global email options and it is used by other extensions (like Forms) to send emails.',
+	'fw'
+);
+
+$manifest['version']     = '1.2.12';
+$manifest['display']     = false;
+$manifest['standalone']  = false;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Mailer-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Mailer-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
